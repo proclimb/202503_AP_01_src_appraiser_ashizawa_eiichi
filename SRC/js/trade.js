b779678085751b2e94a5d0ebcf5e34b2ad3b5036
E.Ashizawa <202503ap01_ashizawa@x-ide.sakura.ne.jp>
2025-05-23 11:37:50 +0900

    #40306 / 業者一覧 / 削除を行うと業者管理一覧に遷移せず、メニューのみの表示となる

--- SRC/js/trade.js
+++ SRC/js/trade.js
@@ -80,11 +80,10 @@ function fnTradeEditCheck() {
 	}
 }
 
-
-
 function fnTradeDeleteCheck(no) {
 	if (confirm('削除します。よろしいですか？')) {
 		form.tradeNo.value = no;
+		form.act.value = 'tradeDelete';
 		form.submit();
 	}
 }
